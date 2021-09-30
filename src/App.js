61028884badf8b3e21f6c0865e2935acff0034a7
JS2K8 <j.p.sharrock1@gmail.com>
2021-09-30 11:27:56 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    handleFetch()
+  }, [])
+
+  const handleFetch = async () => {
+    let response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+    let data = await response.json()
+    setData(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Useless Facts</h1>    
+      <button className="btn" onClick={handleFetch}>FACT!</button>
+      <p className="fact" >{data.text}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
